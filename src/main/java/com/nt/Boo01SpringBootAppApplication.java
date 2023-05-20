71b059309ccb35344511b8f4e9bcd5dd18b78748
Ashok IT <jenadebashis627@gmail.com>
2023-05-20 23:05:54 +0530

    Project Initial Commit

---
+++ src/main/java/com/nt/Boo01SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boo01SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boo01SpringBootAppApplication.class, args);
+	}
+
+}
